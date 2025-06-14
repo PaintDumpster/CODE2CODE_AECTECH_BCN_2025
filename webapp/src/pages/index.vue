cbc7a527ca51cdf466f8c7de64481b2c1002f5fd
alexandrakraeva <alexandra.work.kraeva@gmail.com>
2025-06-14 22:05:26 +0200

    added new placeholders-buttons

--- webapp/src/pages/index.vue
+++ webapp/src/pages/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="d-flex">
-    <div style="width: 25%;">
-      <ChatWindow_Completed />
+  <div>
+    <!-- Top Bar: IFC Upload and View Switcher -->
+    <div class="d-flex align-center mb-4" style="gap: 16px;">
+      <!-- IFC Upload Button -->
+      <label class="v-btn v-btn--outlined v-btn--density-default" style="cursor:pointer;">
+        <input type="file" accept=".ifc" style="display:none" />
+        Upload IFC File
+      </label>
+      <!-- View Switcher -->
+      <v-btn-toggle v-model="viewMode" mandatory>
+        <v-btn value="graph">Graph View</v-btn>
+        <v-btn value="model">3D Model View</v-btn>
+        <v-btn value="overlap">Overlap View</v-btn>
+      </v-btn-toggle>
     </div>
-
-    <div style="width: 75%;">
-      <GraphViewer />
+    <div class="d-flex">
+      <div style="width: 25%;">
+        <ChatWindow_Completed />
+      </div>
+      <div style="width: 75%;">
+        <GraphViewer />
+      </div>
     </div>
   </div>
-  
 </template>
 
 <script setup>
+import { ref } from 'vue'
+const viewMode = ref('graph')
 </script>
